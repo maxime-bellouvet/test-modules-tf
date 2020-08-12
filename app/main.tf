31e873e3d5396c744d32f81cf2edc03811f41083
maxime-bellouvet <49635647+maxime-bellouvet@users.noreply.github.com>
2020-08-12 17:57:05 +0200

    Update main.tf

--- app/main.tf
+++ app/main.tf
@@ -1,17 +1,26 @@
 # infrastructure-modules/app/main.tf
-provider "aws" {
-  region = "us-east-1"
-  # ... other provider settings ...
-}
 terraform {
-  backend "s3" {}
+  required_version = ">= 0.12, < 0.13"
 }
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+
+resource "aws_s3_bucket" "cicd-test-terraform" {
+  bucket = var.BUCKET_NAME
   acl    = "private"
+  region = var.AWS_REGION
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "var.BUCKET_NAME
   }
 }
