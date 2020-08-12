9d9ec9a2e7ce21d89c402989607905d58e15d2e9
maxime-bellouvet <49635647+maxime-bellouvet@users.noreply.github.com>
2020-08-12 16:37:46 +0200

    Update main.tf

--- app/main.tf
+++ app/main.tf
@@ -7,7 +7,6 @@ terraform {
   backend "s3" {}
 }
 module "app" {
-  source = "../../../app"
   instance_type  = var.instance_type
   instance_count = var.instance_count
   # ... other app settings ...
