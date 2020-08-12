09f8ad5dabfea3ba501d06eca3973d973e0ab6aa
maxime-bellouvet <49635647+maxime-bellouvet@users.noreply.github.com>
2020-08-12 14:55:38 +0200

    Create main.tf

---
+++ s3/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+provider "aws" {
+  region     = "eu-west-1"
+}
+resource "aws_s3_bucket" "test" {
+  bucket = "super-bucket-test-cicd"
+  acl    = "private"
+  region = "eu-west-1"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "super-bucket-test-cicd"
+  }
+}
