98e485111e9d0b1714a18354374931db81f3c414
maxime-bellouvet <49635647+maxime-bellouvet@users.noreply.github.com>
2020-08-13 14:54:47 +0200

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,8 +2,13 @@ terraform {
   required_version = ">= 0.12, < 0.13"
 }
 
-resource "aws_instance" "server1" {
-   ami           = "ami-045fa58af83eb0ff4"
-   instance_type = "t2.micro"
-   key_name      = "admin"
- }
+resource "aws_instance" "ec2-jenkins-slave" {
+  ami             = var.AMI
+  instance_type   = var.INSTANCE_TYPE
+  key_name        = var.KEYPAIR_NAME
+  subnet_id       = var.SUBNETID
+  vpc_security_group_ids = [var.SECURITYGROUP]
+  tags = {
+    Name = "Hello world by CICD 17072020"
+  }
+}
